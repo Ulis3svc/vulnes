e9bf329996b8d021b040c3afc5d0e053f9e036ab
root <root@localhost.localdomain>
2019-04-08 15:34:04 -0500

    °_°

--- clase2/prog2.c
+++
@@ -1,8 +0,0 @@
-#include<string.h>
-int main(int argc, char **argv){
-	char i[10];
-	strcpy(i,*(argv+1));
-	//print 
-}
-
-//      r `python -c "print('A'*18)"`
--- clase5/getenv.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv){
-	char *addr;
-	addr = getenv(argv[1]);
-	printf("%s está en %p\n", argv[1],addr);
-}
